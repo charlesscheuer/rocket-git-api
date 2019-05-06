709072922d67027fe1b919ed4d791b5371a0bcdc
charlesscheuer <cscheuer@wustl.edu>
2019-05-06 19:05:16 -0400

    added port

--- server.js
+++ server.js
@@ -1,46 +1,53 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
 const knex = require('knex')
 
 const db = knex({
-    client: 'pg',
-    connection: {
-      host : '127.0.0.1', // which is the same as localhost
-      user : 'frankscheuer', // the user who made the database
-      password : '',
-      database : 'rocket'
-    }
-});
+  client: 'pg',
+  connection: {
+    host: '127.0.0.1', // which is the same as localhost
+    user: 'frankscheuer', // the user who made the database
+    password: '',
+    database: 'rocket'
+  }
+})
 
-const app = express();
-app.use(bodyParser.json());
-app.use(cors());
+const app = express()
+app.use(bodyParser.json())
+app.use(cors())
 
-app.post('/register', (req, res)=> {
-    const { email } = req.body;
-    db('email')
+app.post('/register', (req, res) => {
+  const { email } = req.body
+  db('email')
     .insert({
-        email: email
+      email: email
     })
     .then(() => {
-        res.json('success')
+      res.json('success')
     })
-    .catch(err => res.status(400).json('Unable to register, please try again. If the problem persists please contact us.'))
+    .catch(err =>
+      res
+        .status(400)
+        .json(
+          'Unable to register, please try again. If the problem persists please contact us.'
+        )
+    )
 })
 
 app.get('/count', (req, res) => {
-    db.select('*').from('email')
+  db.select('*')
+    .from('email')
     .then(emails => {
-        return emails.length
+      return emails.length
     })
     .then(count => {
-        res.json(count)
+      res.json(count)
     })
     .catch(() => res.json('450'))
 })
 
-app.listen(3000, () => {
-    console.log('listening')
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`listening on: ${process.env.PORT}`)
 })
