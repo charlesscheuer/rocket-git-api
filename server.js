37306e61b28893c66cb3394beadd298a9c7ad8f4
charlesscheuer <cscheuer@wustl.edu>
2019-05-06 22:12:23 -0400

    database connection

--- server.js
+++ server.js
@@ -11,10 +11,8 @@ app.use(cors())
 const db = knex({
   client: 'pg',
   connection: {
-    host: '127.0.0.1', // which is the same as localhost
-    user: 'frankscheuer', // the user who made the database
-    password: '',
-    database: 'rocket'
+    connectionString: process.env.DATABASEURL,
+    ssl: true
   }
 })
 
